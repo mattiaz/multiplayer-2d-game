ee995ba50f8b44f5d3e87cd2c1f1bb199c180ea3
Mattias <mattias@ajander.se>
2014-10-29 13:57:06 +0100

    Changed some required packages to the project and added some code to server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+//
+//  REQUIRE
+//
+
+var ws_module = require('websocket').server;
+var http_module = require('http');
+var fs_module = require('fs');
+var package_json = require(./package.json);
+
+//
+//  SETTINGS
+//
+
+var settings = {
+    app: {
+        name: package_json.name,
+        version: package_json.version
+    },
+    http: {
+        port: 80
+    }
+};
